dd158b12e7baede473daa306d76481f86eae6835
HojdaAdelin <adelinionut297@gmail.com>
2022-10-25 10:30:27 +0300

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
-#include "src/system/login.h"
-#include "src/system/register.h"
+#include "../src/system/login.h"
+#include "../src/system/register.h"
 
 using namespace std;
 
 int main(){
 
+    int option;
+
+    cout << "1. Log in";
+    cout << "\n2. Register" << endl;
+
+    cin >> option;
+
+    switch (option)
+    {
+    case 1:
+        login();
+        break;
+    case 2:
+        Register();
+        break;
+    default:
+        system("pause");
+        break;
+    }
 
     return 0;
 }
